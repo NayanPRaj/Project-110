d456a8bf67feeb361657f9567ce5cee488a3b6b6
Nayan <74858861+NayanPRaj@users.noreply.github.com>
2022-01-16 17:01:18 +0530

    Add files via upload

---
+++ Project-110.py
@@ -0,0 +1,56 @@
+import statistics
+import pandas as pd
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import random
+import csv
+
+df = pd.read_csv("C-110.csv")
+data = df["average"].tolist()
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+#function to plot the mean on the graph
+def show_fig(mean_list):
+    df = mean_list
+    mean = statistics.mean(df)
+    fig = ff.create_distplot([df], ["temp"], show_hist=False)
+    fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 1], mode="lines", name="MEAN"))
+    fig.show()
+
+    ##code to find the mean of the raw data ("population data")
+population_mean = statistics.mean(data)
+print("Mean population:- ", population_mean)
+
+def setup():
+    mean_list = []
+    for i in range(0,1000):
+        set_of_means= random_set_of_mean(100)
+        mean_list.append(set_of_means)
+    show_fig(mean_list)
+    
+    mean = statistics.mean(mean_list)
+    print("Mean of sampling distribution :-",mean )
+
+
+# code to find the standard deviation of the sample data
+def standard_deviation():
+    mean_list = []
+    for i in range(0,1000):
+        set_of_means= random_set_of_mean(100)
+        mean_list.append(set_of_means)
+
+    std_deviation = statistics.stdev(mean_list)
+    print("Standard deviation of sampling distribution:- ", std_deviation)
+
+setup()
+standard_deviation()
+
+
